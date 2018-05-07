dda0dbca7f3f5002f7347e51b429593998621037
Ricardo Quesada <ricardoquesada@gmail.com>
2018-05-07 07:16:06 -0700

    commands

--- src/banner.asm
+++ src/banner.asm
@@ -49,58 +49,14 @@ banner_start:
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 banner_init:
-        push    ds
-        push    es
-
         mov     ax,banner_irq_8
         call    irq_8_init
 
         mov     ax,0x0004                       ;320x200 4 colors
         int     0x10
 
-        mov     cx,20
-.loop:
-        push    cx
-
-        ;--- P ---
-        mov     ax,banner_data                  ;ds:si (source)
-        mov     ds,ax
-        mov     si,letter_p_lz4
-
-        mov     ax,0x1800                       ;es:di (destination)
-        mov     es,ax
-        mov     di,0x4000
-
-        call    lz4_decompress
-
-
-        ;--- V ---
-        mov     ax,banner_data                  ;ds:si (source)
-        mov     ds,ax
-        mov     ax,0x1800                       ;es:di (destination)
-        mov     es,ax
-
-        mov     si,letter_v_lz4
-        mov     di,0x4000
-        call    lz4_decompress
-
-
-        ;--- M ---
-        mov     ax,banner_data                  ;ds:si (source)
-        mov     ds,ax
-        mov     ax,0x1800                       ;es:di (destination)
-        mov     es,ax
-
-        mov     si,letter_m_lz4
-        mov     di,0x4000
-        call    lz4_decompress
-
-        pop     cx
-        loop    .loop
-
-        pop     es
-        pop     ds
         ret
+
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 banner_cleanup:
@@ -111,6 +67,13 @@ banner_cleanup:
 banner_main_loop:
 
 .loop:
+        call    key_pressed
+        jz      .loop
+
+        ret
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+key_pressed:
 %if EMULATOR
         cli                                     ;on emulator, test for keyboard buffer
 
@@ -119,8 +82,8 @@ banner_main_loop:
 
         sub     ax,ax
         mov     ds,ax                           ;ds = zero page
-        mov     ax, [0x41a]                     ;keyboard buffer head
-        cmp     ax, [0x41c]                     ;keyboard buffer tail
+        mov     ax,[0x041a]                     ;keyboard buffer head
+        cmp     ax,[0x041c]                     ;keyboard buffer tail
 
         pop     ax
         pop     ds
@@ -129,18 +92,46 @@ banner_main_loop:
         in      al,0x62                         ;on real hardware, test keystroke missed?
         and     al,1                            ; so that we can disable IRQ9
 %endif
-        jz      .loop
-
-        int 3
-
         ret
+
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 banner_irq_8:
+
+        call    play_music
+        call    update_state_machine
+
         mov     al,0x20                         ;send the EOI signal
         out     0x20,al                         ; to the IRQ controller
 
         iret
+
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+play_music:
+        ret
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+update_state_machine:
+        ret
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; uncompress letter to 1800:4000
+; input: ax=letter to uncompress
+uncompress_letter:
+
+        ;assert (ds=banner_data)
+        shl     ax,1                            ;each address takes 2 bytes
+        mov     bx,ax                           ;uses bx for index
+        mov     si,[letter_idx + bx]            ;ds:si: compressed data
+
+        mov     ax,0x1800                       ;es:di (destination)
+        mov     es,ax
+        mov     di,0x4000
+
+        call    lz4_decompress
+
+        ret
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ;
@@ -148,10 +139,123 @@ banner_irq_8:
 ;
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 section .banner_data data
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; compress data
 letter_p_lz4:
         incbin 'src/p.raw.lz4'
 letter_v_lz4:
         incbin 'src/v.raw.lz4'
 letter_m_lz4:
         incbin 'src/m.raw.lz4'
+letter_invites_lz4:
+        incbin 'src/invites.raw.lz4'
+letter_you_lz4:
+        incbin 'src/you.raw.lz4'
+letter_to_lz4:
+        incbin 'src/to.raw.lz4'
+letter_flashparty_lz4:
+        incbin 'src/fp.raw.lz4'
+letter_2018_lz4:
+        incbin 'src/2018.raw.lz4'
 
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; pointers to the data. each one has its own index
+letter_idx:
+        dw letter_p_lz4                         ;0
+        dw letter_v_lz4                         ;1
+        dw letter_m_lz4                         ;2
+        dw letter_invites_lz4                   ;3
+        dw letter_you_lz4                       ;4
+        dw letter_you_lz4                       ;5
+        dw letter_to_lz4                        ;6
+        dw letter_flashparty_lz4                ;7
+        dw letter_2018_lz4                      ;8
+
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; available tokens
+DISPLAY         equ 0                             ;letter to display
+FLASH           equ 1
+DELAY           equ 2
+BLACK           equ 3
+END             equ 4
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; available tokens
+commands:
+        db DELAY,5
+        db DISPLAY,0                            ;p
+        db DELAY,100
+        db DISPLAY,1                            ;v
+        db DELAY,100
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,100
+
+        db DISPLAY,0                            ;p
+        db DELAY,75
+        db DISPLAY,1                            ;v
+        db DELAY,75
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,75
+
+        db DISPLAY,0                            ;p
+        db DELAY,50
+        db DISPLAY,1                            ;v
+        db DELAY,50
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,50
+
+        db DISPLAY,0                            ;p
+        db DELAY,25
+        db DISPLAY,1                            ;v
+        db DELAY,25
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,25
+
+        db DISPLAY,0                            ;p
+        db DELAY,5
+        db DISPLAY,1                            ;v
+        db DELAY,5
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,5
+
+        db DISPLAY,0                            ;p
+        db DELAY,5
+        db DISPLAY,1                            ;v
+        db DELAY,5
+        db DISPLAY,2                            ;m
+        db BLACK
+        db DELAY,5
+
+        db DISPLAY,4                            ;invites
+        db DELAY,30
+        db DISPLAY,5                            ;you
+        db DELAY,30
+        db DISPLAY,6                            ;to
+        db DELAY,30
+
+        db DISPLAY,7                            ;flash party
+        db DELAY,30
+        db FLASH
+        db DELAY,5
+        db FLASH
+        db DELAY,5
+        db FLASH
+        db DELAY,5
+        db FLASH
+        db DELAY,5
+        db FLASH
+        db DELAY,5
+        db FLASH
+        db DELAY,30
+
+        db DISPLAY,8                            ;2018
+        db DELAY,150
+
+        db END
