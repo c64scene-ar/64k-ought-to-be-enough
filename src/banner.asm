0c8f97195a49dd3391a25addfb720b638b20f82c
Ricardo Quesada <ricardoquesada@gmail.com>
2018-05-14 08:38:31 -0700

    music while displaying banner

--- src/banner.asm
+++ src/banner.asm
@@ -54,6 +54,7 @@ banner_init:
 
         mov     ax,banner_irq_8
         call    irq_8_init
+        call    music_init
 
         mov     ax,0x0004                       ;320x200 4 colors
         int     0x10
@@ -139,7 +140,7 @@ banner_irq_8:
         mov     ax,0x1800
         mov     es,ax
 
-        call    play_music
+        call    music_play
         call    update_state_machine
 
         mov     al,0x20                         ;send the EOI signal
@@ -159,8 +160,96 @@ banner_irq_8:
 
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
-play_music:
+music_init:
+        in      al,0x61                         ;PCJr only:
+        or      al,0b0110_0000                  ; source for music is the SN76496
+        out     0x61,al
+
+        mov     word [pvm_offset],pvm_song + 0x10       ;update start offset
+        sub     al,al
+        mov     byte [pvm_wait],al              ;don't wait at start
         ret
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+music_play:
+
+MUSIC_DATA              equ 0b0000_0000
+MUSIC_DATA_EXTRA        equ 0b0010_0000
+MUSIC_DELAY             equ 0b0100_0000
+MUSIC_DELAY_EXTRA       equ 0b0110_0000
+MUSIC_END               equ 0b1000_0000
+
+        sub     cx,cx                           ;cx=0... needed later
+        mov     si,[pvm_offset]
+
+        cmp     byte [pvm_wait],0
+        je      .l0
+
+        dec     byte [pvm_wait]
+        ret
+
+.l0:
+        lodsb                                   ;fetch command byte
+        mov     ah,al
+        and     al,0b1110_0000                  ;al=command only
+        and     ah,0b0001_1111                  ;ah=command args only
+
+        cmp     al,MUSIC_DATA                   ;data?
+        je      .is_data
+        cmp     al,MUSIC_DATA_EXTRA             ;data extra?
+        je      .is_data_extra
+        cmp     al,MUSIC_DELAY                  ;delay?
+        je      .is_delay
+        cmp     al,MUSIC_DELAY_EXTRA            ;delay extra?
+        je      .is_delay_extra
+        cmp     al,MUSIC_END                    ;end?
+        je      .is_end
+
+.unsupported:
+        int 3
+        mov     [pvm_offset],si                 ;save offset
+        ret
+
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+.is_data:
+        mov     cl,ah                           ;ch is already zero
+        jmp     .repeat
+
+.is_data_extra:
+        lodsb                                   ;fetch lenght from next byte
+        mov     cl,al
+.repeat:
+        lodsb
+        out     0xc0,al                         ;play music
+
+        loop    .repeat
+
+        jmp     .l0                             ;repeat... fetch next command
+
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+.is_delay:
+        dec     ah                              ;minus one, since we are returning
+        mov     [pvm_wait],ah                   ; from here now
+        mov     [pvm_offset],si
+        ret
+
+.is_delay_extra:
+        lodsb                                   ;fetch wait from next byte
+        dec     al                              ;minus one, since we are returning
+        mov     [pvm_wait],al                   ; from here now
+        mov     [pvm_offset],si
+        ret
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+.is_end:
+        int 3
+        mov     ax,[pvm_song + 0xc]             ;offset loop relative to start of data
+        add     ax,pvm_song + 0x10              ;add header size
+        mov     word [pvm_offset],ax            ;update new offset with loop data
+        ret
+
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 update_state_machine:
@@ -281,6 +370,21 @@ letter_flashparty_lz4:
         incbin 'src/fp.raw.lz4'
 letter_2018_lz4:
         incbin 'src/2018.raw.lz4'
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; song related
+pvm_song:
+        incbin 'src/uctumi-song.pvm'
+pvm_wait:                                       ;cycles to read divided 0x2df
+        db 0
+pvm_offset:                                     ;pointer to next byte to read
+        dw 0
+volume_0:
+        db      0b1001_1111                     ;vol 0 channel 0
+        db      0b1011_1111                     ;vol 0 channel 1
+        db      0b1101_1111                     ;vol 0 channel 2
+        db      0b1111_1111                     ;vol 0 channel 3
+VOLUME_0_MAX equ $ - volume_0
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; vars
