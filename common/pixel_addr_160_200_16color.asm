578b320ec6c6800784e360174b2acda4696c799e
Ricardo Quesada <ricardoquesada@gmail.com>
2018-09-01 19:26:00 -0700

    non working...

--- common/pixel_addr_160_200_16color.asm
+++ common/pixel_addr_160_200_16color.asm
@@ -2,10 +2,10 @@
 ;
 ; Name:     PixelAddr16
 ;
-; Function: Determine buffer address of pixel in 320x200 4-color mode
+; Function: Determine buffer address of pixel in 160x100 16-color mode
 ;
-; Caller:       AX = y-coordinate (0-199)
-;               BX = x-coordinate (0-319)
+; Caller:       AX = y-coordinate (0-99)
+;               BX = x-coordinate (0-159)
 ;
 ; Returns:      AH = bit mask
 ;               BX = byte offset in buffer
@@ -20,15 +20,49 @@
 ;
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ;
-; Adapted to nasm and, PCjr's 320x200 16-color: riq
+; Adapted to nasm and, PCjr's 160x200 16-color: riq
 ;
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 
+; 2 pixels per byte
+; 160 / 2 = 80 bytes per horizontal line. (80 = 64 + 16)
+; offset = y * 80 + x / 2
+; cl = (x & 1) ? 0 : 1
 
 OriginOffset    equ 0       ; byte offset of (0,0)
 VideoBufferSeg  equ 0xb800
 
 PixelAddr04:
+	int 3
+	mov 	cx,0xb800
+	mov 	es,cx
+
+	; y * 80
+	mov 	dx,ax 		;save Y for later
+	mov 	cl,4 		;multiply by 16
+	shl 	ax,cl		; ax = y * 16
+	mov 	cl,6 		;multiply by 64
+	shl 	dx,cl 	 	; dx = y * 64
+	add 	ax,dx 		;ax = ax + dx  (ax = y * 80)
+
+	; calculate x
+	shr 	bx,1 		;x = x / 2, and useful to carry set to calc. mask
+	jc 	is_odd
+
+is_even:
+	add 	bx,ax 		; offset = y * 80 + x / 2
+	mov 	ah,0b1111_0000	;mask
+	mov 	cl,4 		;bits to shift
+	ret
+
+is_odd:
+	add 	bx,ax 		; offset = y * 80 + x / 2
+	mov 	ah,0b0000_1111 	;mask
+	sub	cl,cl		;bits to shift
+	ret
+	
+
+PixelAddr04_old:
         mov     cl,bl       ; CL := low-order byte of x
 
         xchg    ah,al       ; AX := 0x100 * y
